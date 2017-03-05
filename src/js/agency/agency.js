0185f90fe27056cb6708a5265c987f31a5eda073
mutong wu <mutong wu>
2017-03-05 23:18:20 +0800

    update

---
+++ src/js/agency/agency.js
@@ -0,0 +1,17 @@
+$(function(){
+	var pager = {
+		init: function(){
+			new App.PageBar({
+				el: $('#j_pageBox'),
+				cls: 'ui_pageBar_L',
+				totalNum: 40,
+				jumpTo: false
+			})
+		}
+	}
+
+	if($('.recordCnt').size()){
+		pager.init();	
+	}
+	
+})
