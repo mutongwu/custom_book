2486acdc699f0b6480ce387b892de98bc2914e52
shawnwu <shawnwu@qq.com>
2017-03-10 21:50:55 +0800

    update

--- src/fis-conf.js
+++ src/fis-conf.js
@@ -1,10 +1,10 @@
 
-// fis.hook('relative');
+fis.hook('relative');
 
 // 让所有文件，都使用相对路径。
-// fis.match('**', {
-//      relative: true
-// });
+fis.match('**', {
+     relative: true
+});
 
 fis.match('*.bat', {
     release: false
@@ -14,7 +14,6 @@ fis.match('*.scss', {
     parser: fis.plugin('node-sass'),
     rExt: '.css'
 });
-
 // fis.media('debug').match('*.{js,css,png}', {
 //     useHash: false,
 //     useSprite: false,
