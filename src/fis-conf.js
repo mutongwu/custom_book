69af2e62fac756da5afb98e73fe43de2bee5ffef
shawnwu <shawnwu@qq.com>
2018-03-17 18:39:06 +0800

    update pakcage

--- src/fis-conf.js
+++ src/fis-conf.js
@@ -1,21 +1,38 @@
 
-fis.hook('relative');
+// fis.hook('relative');
 
-// 让所有文件，都使用相对路径。
-fis.match('**', {
-     relative: true
-});
-
-fis.match('*.bat', {
-    release: false
-});
 
 fis.match('*.scss', {
     parser: fis.plugin('node-sass'),
     rExt: '.css'
 });
-// fis.media('debug').match('*.{js,css,png}', {
-//     useHash: false,
-//     useSprite: false,
-//     optimizer: null
-// });
+
+fis.match('package*.json', {
+  release: false
+});
+
+
+fis.media('prod').match('*.js', {
+  optimizer: fis.plugin('uglify-js')
+}).match('*.{js,css,png,jpeg,jpg}', {
+  useHash: true
+}).match('*', {
+    domain: 'http://cdn.baidu.com/dist'
+}).match('::package', {
+  postpackager: fis.plugin('loader', {
+    allInOne: true
+  })
+}).match('lib/*.js', {
+  packTo: '/pkg/aio.js',
+  packOrder: -50
+}).match('widgets/*.js', {
+  packTo: '/pkg/aio.js'
+}).match('jquery-1.11.3.min.js', {
+  packOrder: -100
+}).match('common.js', {
+  packOrder: -10
+}).match('css/{common.css,style.scss,bookfx.css}', {
+  packTo: '/pkg/aio.css'
+}).match('common.css', {
+  packOrder: -100
+});
