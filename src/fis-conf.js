ba725fdeef2a1e716775a8b22ac102301f5f1329
shawnwu <shawnwu@qq.com>
2018-03-18 11:40:12 +0800

    update 路径

--- src/fis-conf.js
+++ src/fis-conf.js
@@ -1,7 +1,4 @@
 
-// fis.hook('relative');
-
-
 fis.match('*.scss', {
     parser: fis.plugin('node-sass'),
     rExt: '.css'
@@ -11,14 +8,12 @@ fis.match('package*.json', {
   release: false
 });
 
+fis.match('*.{png,js,css,jpeg,jpg,gif}', {
+  release: '/static/$0'
+});
 
-fis.media('prod').match('*.js', {
-  optimizer: fis.plugin('uglify-js')
-}).match('*.{js,css,png,jpeg,jpg}', {
-  useHash: true
-}).match('*', {
-    domain: 'http://cdn.baidu.com/dist'
-}).match('::package', {
+/*打包配置*/
+fis.match('::package', {
   postpackager: fis.plugin('loader', {
     allInOne: true
   })
@@ -35,4 +30,40 @@ fis.media('prod').match('*.js', {
   packTo: '/pkg/aio.css'
 }).match('common.css', {
   packOrder: -100
+});
+
+fis.match('**', {
+    deploy: [
+        fis.plugin('local-deliver')
+    ]
+});
+
+/*页面访问的根路径(替换a标签页面 href属性)*/
+fis.match('*.html', {
+    deploy: [
+        fis.plugin('replace', {
+            from: '@ROOT_PATH',
+            to: ''
+        }),
+        fis.plugin('local-deliver')
+    ]
+});
+
+/*===================发布配置====================*/
+fis.media('prod').match('*.js', {
+  optimizer: fis.plugin('uglify-js')
+}).match('*.css', {
+  optimizer: fis.plugin('clean-css')
+}).match('*.{js,css,png,jpeg,jpg,gif}', {
+  useHash: true
+}).match('*', {
+    // domain: '//www.sy111.com/book'
+}).match('*.html', {
+    deploy: [
+        fis.plugin('replace', {
+            from: '@ROOT_PATH',
+            to: ''
+        }),
+        fis.plugin('local-deliver') //must add a deliver, such as http-push, local-deliver
+    ]
 });
